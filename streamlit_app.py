8b100c62c59987bf10c66bcdd2e68bb3214867f2
cchloe2 <94399704+cchloe2@users.noreply.github.com>
2023-04-21 15:59:11 +0200

    Update streamlit_app.py
    Remove the double list

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,5 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-streamlit.dataframe(my_fruit_list)
-
 streamlit.multiselect("Pick some fruits :", list(my_fruit_list))
 streamlit.dataframe(my_fruit_list)
