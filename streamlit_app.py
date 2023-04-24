3a52d9746c839b9e8e9425f6221ab11b5c812e6e
cchloe2 <94399704+cchloe2@users.noreply.github.com>
2023-04-24 16:30:24 +0200

    Update streamlit_app.py
    Removed pandas library

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,6 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
-
 streamlit.title('My parents New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
@@ -30,7 +27,7 @@ streamlit.write('The user entered ', fruit_choice)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 streamlit.stop()
